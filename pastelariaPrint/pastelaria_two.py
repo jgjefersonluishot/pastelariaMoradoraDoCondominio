9f4ac212209b44d248f39dba230e8cf7c1dfe9e8
Jeferson Luis Moraes Gonçalves <jgjefersonluishot@hotmail.com>
2025-09-29 23:46:26 -0300

    Create pastelaria_two
    Segunda ideia para desenvolvimento usando comando print

---
+++ pastelariaPrint/pastelaria_two.py
@@ -0,0 +1,19 @@
+print("====================================================")
+print("          PASTÉIS! Moradora do condomínio!           ")
+print("====================================================")
+
+print("Vendo saborosos pastéis bem recheados.")
+print("Valor R$3,50 cada.\n")
+
+print("Sabores dos pastéis:")
+print(" - Carne")
+print(" - Frango")
+print(" - Queijo")
+print(" - Presunte e Queijo")
+print(" - Mumu (Doce de leite)\n")
+
+print("Me chama no WhatsApp:")
+print("(51) 99603-1881\n")
+
+print("Aceitamos encomendas!")
+print("=====================================")
