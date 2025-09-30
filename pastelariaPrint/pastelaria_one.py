9175efe3b80a811cb9f2e5642c018f4e6062c642
Jeferson Luis Moraes Gonçalves <jgjefersonluishot@hotmail.com>
2025-09-29 23:40:59 -0300

    console comando print
    Programa desenvolvido todo com o comando print.

---
+++ pastelariaPrint/pastelaria_one.py
@@ -0,0 +1,19 @@
+print("==========================================")
+print("             NOSSOS PASTÉIS!              ")
+print("==========================================")
+print("Pessoal, sou moradora do condomínio")
+print("e vendo saborosos pastéis bem recheados")
+print("por R$3,50 cada.\n")
+
+print("Sabores dos pastéis:")
+print(" - Carne")
+print(" - Frango")
+print(" - Queijo")
+print(" - Mumu\n")
+
+print("Contatos:")
+print(" (51) 3237-8576  |  (51) 99603-1881\n")
+
+print("Temos grupo no WhatsApp, me chame por lá")
+print("que incluo você. Aceitamos encomendas!")
+print("==========================================")
