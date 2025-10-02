d6d5a04c7ce909d1d9d395187d27a53bb21682a0
Jeferson Luis Moraes Gonçalves <jgjefersonluishot@hotmail.com>
2025-10-02 12:48:43 -0300

    create function fazer pedido
    função fazer pedido referencia da compra, quantidade e valor total.

--- pastelariaInput/pastelariaInputOne.py
+++ pastelariaInput/pastelariaInputOne.py
@@ -1,5 +1,5 @@
 import locale
-import locate
+
 
 # Config e formatação moeda Brasil.
 locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
@@ -10,7 +10,38 @@ PRECO_PASTEL = 3.50
 # Lista de sabores disponiveis
 sabores = ["Carne", "Frango", "Queijo", "Presundo e Queijo", "Mumu (Doce de Leite)"]
 
-print(sabores)
+# Fazer pedido
+def fazer_pedido():
+    print("\n=============== FAÇA O SEU PEDIDO ===================")
+    nome = input("Digite o seu nome: ")
+    whatsapp = input("Digite o seu WhatsApp: ")
+    endereco = input("Digite o seu endereço: ")
+
+    print("\nSabores disponíveis: ")
+    for i, sabor in enumerate(sabores, start=1):
+        print(f"{1}. {sabor}")
+
+    escolha = int(input("\nEscolha o número do sabor: "))
+    if 1<= escolha <= len(sabores):
+        sabor_escolhido = sabores[escolha - 1]
+    else:
+        print("Opção inválida! Pedido Cancelado.")
+
+    quantidade = int(input("Quantos pastéis deseja? "))
+
+    valor_total = quantidade * PRECO_PASTEL
+    valor_formatado = locale.currency(valor_total,grouping=True)
+
+    print("\n=============== RESUMO DO PEDIDO ===================")
+    print(f"Cliente: {nome}")
+    print(f"WhatsApp: {whatsapp}")
+    print(f"Endereço: {endereco}")
+    print(f"Sabor escolhido: {sabor_escolhido}")
+    print(f"Quantidade: {quantidade}")
+    print(f"Valor total: {valor_formatado}")
+    print("\n=====================================================")
+
+
 
 
 
