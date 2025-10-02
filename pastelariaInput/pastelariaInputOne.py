70a4dd0d90f9a2a7b383ad2bcdc0321688ecff4e
Jeferson Luis Moraes Gonçalves <jgjefersonluishot@hotmail.com>
2025-10-02 11:21:00 -0300

    Create pastelariaInputOne.py
    iniciando os trabalhos, com configuração e formatação (moeda brasileira).
    Preços e lista.

---
+++ pastelariaInput/pastelariaInputOne.py
@@ -0,0 +1,17 @@
+import locale
+import locate
+
+# Config e formatação moeda Brasil.
+locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
+
+# Preço do pastel
+PRECO_PASTEL = 3.50
+
+# Lista de sabores disponiveis
+sabores = ["Carne", "Frango", "Queijo", "Presundo e Queijo", "Mumu (Doce de Leite)"]
+
+print(sabores)
+
+
+
+
