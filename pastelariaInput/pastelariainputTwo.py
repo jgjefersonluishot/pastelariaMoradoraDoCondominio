4c3f8b47d810c100baab394eeef1c2b07fe1e8b1
Jeferson Luis Moraes Gonçalves <jgjefersonluishot@hotmail.com>
2025-10-06 15:44:32 -0300

    Create pastelariainputTwo.py
    Criação e configuração e variaveis.

---
+++ pastelariaInput/pastelariainputTwo.py
@@ -0,0 +1,15 @@
+import locale
+from datetime import datetime
+
+from pastelariaInput.pastelariaInputOne import PRECO_PASTEL
+
+# Configura a moeda brasileira
+locale.setlocale(locale.LC_ALL,"pt_BR.UTF=8")
+
+# Preço do pastel
+PRECO_PASTEL=3.50
+
+# Lista de sabores disponiveis
+sabores = ["Carne", "Frango", "Queijo", "Presunto e Queijo", "Mumu (Doce de leite)"]
+
+# funções
