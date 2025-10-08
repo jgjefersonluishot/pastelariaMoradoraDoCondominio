e4850ee964f7aa2bf7faa3b30ec4fd5666358d2d
Jeferson Luis Moraes Gonçalves <jgjefersonluishot@hotmail.com>
2025-10-07 23:33:37 -0300

    Update pastelariainputTwo.py
    iniciando a função fazer pedido, inserindo as variaveis e o loop para escolha do sabores

--- pastelariaInput/pastelariainputTwo.py
+++ pastelariaInput/pastelariainputTwo.py
@@ -13,3 +13,15 @@ PRECO_PASTEL=3.50
 sabores = ["Carne", "Frango", "Queijo", "Presunto e Queijo", "Mumu (Doce de leite)"]
 
 # funções
+
+
+def fazer_pedito():
+    print("\n=============== FAÇA O SEU PEDIDO =================")
+    atendente = input("Nome do atendente: ")
+    nome = input("Nome do cliente: ")
+    whatsapp = input("WhatsApp do cliente: ")
+    endereco = input("Endereço do cliente: ")
+
+    print("\nSabores disponiveis: ")
+    for i, sabor in enumerate(sabores, start=1):
+        print(f"{i}. {sabor}")
