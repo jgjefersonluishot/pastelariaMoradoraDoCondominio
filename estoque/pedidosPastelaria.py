684377b008b992e996aa51659e55502f2ccf366d
Jeferson Luis Moraes Gonçalves <jgjefersonluishot@hotmail.com>
2025-11-03 16:15:26 -0300

    Update pedidosPastelaria.py
    Preços do pastel e bebidas.

--- estoque/pedidosPastelaria.py
+++ estoque/pedidosPastelaria.py
@@ -5,3 +5,14 @@ from datetime import datetime
 
 # Configura moeda brasileira
 locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
+
+# Preços
+PRECO_PASTEL = 3.50
+BEBIDAS = {
+    "Coca-Cola lata": 5.00,
+    "Guaraná lata": 4.50,
+    "Fanta Laranja lata": 4.50,
+    "Fanta Uva lata": 4.50,
+    "Suco Laranja 500ml": 6.00,
+    "Suco Uva 500ml": 6.00
+}
