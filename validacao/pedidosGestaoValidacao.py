b978f88d783f611dc2cc8d1cc3cc58ec27763549
Jeferson Luis Moraes Gonçalves <jgjefersonluishot@hotmail.com>
2025-11-11 15:24:31 -0300

    Create pedidosGestaoValidacao.py
    start na validação das especificações do projeto. Estoque inicial.

---
+++ validacao/pedidosGestaoValidacao.py
@@ -0,0 +1,20 @@
+import datetime
+
+# Estoque inicial
+estoque = {
+    "pasteis": {
+        "Carne": 20,
+        "Frango": 20,
+        "Queijo": 20,
+        "Presunto e Queijo": 20,
+        "Mumu (Doce de leite)": 20
+    },
+    "bebidas": {
+        "Coca-Cola lata": 10,
+        "Guaraná lata": 10,
+        "Fanta Laranja lata": 10,
+        "Fanta Uva lata": 10,
+        "Suco Laranja 500ml": 5,
+        "Suco Uva 500ml": 5
+    }
+}
